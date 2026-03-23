cb4b0309b58cfa12c520ce6e397458132f90c6a4
Al101020 <alex.vl.m@yandex.ru>
2026-03-23 10:37:22 +1000

    commit-0.01

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+import './App.css'
+// import { Provaider } from 'react-redux';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h3>Задание</h3>
+    </>
+  )
+}
+
+export default App
